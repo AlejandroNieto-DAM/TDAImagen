2e6ae664d04db5a4f81c64c726d0321f5fc091cb
AlejandroNieto-DAM <alejandrona5b@gmail.com>
2021-10-15 12:31:48 +0200

    subImage y contraste

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include "imagen.hpp"
-using namespace std;
+#include "imagenES.h"
 
 int main() {
 
 //Se vienen cositas
   Image imagen;
   imagen.printf();
+
+  //subImage("imagenespgmppm/vacas.pgm", "cabeza.pgm", 85, 145, 60, 80);
+  //contraste("imagenespgmppm/vacas.pgm", "espectaculo.pgm", 0, 255, 255, 0);
 
 }
